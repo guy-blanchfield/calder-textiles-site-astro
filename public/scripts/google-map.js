8c6c028f06ab7effdc6f5810ca930a3b8d3395fd
Guy Blanchfield <guyblanchfield@gmail.com>
2023-07-30 15:19:28 +0100

    GoogleMap new implementation done, delete woff preload from Layout

--- public/scripts/google-map.js
+++ public/scripts/google-map.js
@@ -1,14 +1,61 @@
+// Initialize and add the map
 let map;
 
 async function initMap() {
-	const { Map } = await google.maps.importLibrary("maps");
+	// The location of Calder Textiles
+	const caldertextiles = { lat: 53.680525, lng: -1.63789 };
 
+	// Request needed libraries.
+	//@ts-ignore
+	const { Map } = await google.maps.importLibrary("maps");
+	const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+	// The map, centered at Calder Textiles
 	map = new Map(document.getElementById("map"), {
-		center: { lat: -34.397, lng: 150.644 },
-		zoom: 8,
+		zoom: 13,
+		center: caldertextiles,
+		mapId: "CALDER_TEXTILES",
+		// options: {
+		// gestureHandling: "greedy",
+		// },
+		// styling (e.g. night mode) has to be done with the cloud console
+		// https://developers.google.com/maps/documentation/cloud-customization/overview#cloud_tooling
 	});
+
+	// The marker, positioned at Calder Textiles
+	const marker = new AdvancedMarkerElement({
+		map: map,
+		draggable: false,
+		// little drop animation on the marker
+		// now done with css
+		// animation: google.maps.Animation.DROP,
+		position: caldertextiles,
+		title: "Calder Textiles",
+	});
+
+	const content = marker.content;
+
+	content.style.opacity = "0";
+	content.addEventListener("animationend", (event) => {
+		content.classList.remove("drop");
+		content.style.opacity = "1";
+	});
+
+	const delayTime = 1.5;
+	content.style.setProperty("--delay-time", delayTime + "s");
+	// observe the marker content
+	intersectionObserver.observe(content);
 }
 
-console.log(`initialising google map`);
+// now using advancedMarkerElement so animation uses css (in the Astro component) and intersection observer
+const intersectionObserver = new IntersectionObserver((entries) => {
+	for (const entry of entries) {
+		if (entry.isIntersecting) {
+			entry.target.classList.add("drop");
+			intersectionObserver.unobserve(entry.target);
+		}
+	}
+});
 
-initMap();
+setTimeout(initMap, 3000);
+// initMap();
