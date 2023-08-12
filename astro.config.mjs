0c874d268259dcc41bab296a3a2d49d1ec9da3e6
Guy Blanchfield <guyblanchfield@gmail.com>
2023-08-12 23:40:20 +0100

    Added astro-html-minfy to remove html comments on build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import frontendistahtmlMinify from "@frontendista/astro-html-minify";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		frontendistahtmlMinify({
+			// https://github.com/frontendista/astro-html-minify#13-customize-minifiers-options
+			html: {
+				// customise terser options
+				// strip out the comments
+				removeComments: true,
+			},
+		}),
+	],
+});
