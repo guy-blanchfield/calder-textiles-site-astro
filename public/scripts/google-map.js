3fa7c35c6e6c423b009ba1861ff3a590a2df40e9
Guy Blanchfield <guyblanchfield@gmail.com>
2023-08-25 15:51:27 +0100

    Added infoWindow to Google map, js only

--- public/scripts/google-map.js
+++ public/scripts/google-map.js
@@ -6,9 +6,11 @@ async function initMap() {
 	const caldertextiles = { lat: 53.680525, lng: -1.63789 };
 
 	// Request needed libraries.
+	// also access infoWindow here
 	//@ts-ignore
-	const { Map } = await google.maps.importLibrary("maps");
+	const { Map, InfoWindow } = await google.maps.importLibrary("maps");
 	const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+	// will we need to request infoWindow as well?
 
 	// The map, centered at Calder Textiles
 	map = new Map(document.getElementById("map"), {
@@ -45,6 +47,35 @@ async function initMap() {
 	content.style.setProperty("--delay-time", delayTime + "s");
 	// observe the marker content
 	intersectionObserver.observe(content);
+
+	// the infoWindow (opens on clicking the marker)
+
+	//  content for the infoWindow
+	const infoWindowContent = `
+		<h4>Calder Textiles</h4>
+		<p>
+			Dewsbury Mills<br />
+			Thornhill Road<br />
+			Dewsbury<br />
+			WF12 9QE<br />
+			United Kingdom
+		</p>
+	`;
+
+	//  create the infoWindow
+	const infowindow = new InfoWindow({
+		content: infoWindowContent,
+		ariaLabel: "Calder Textiles",
+	});
+
+	// open the infoWindow on marker's click event
+	// NB addListener is a method of google map's event object
+	marker.addListener("click", () => {
+		infowindow.open({
+			anchor: marker,
+			map,
+		});
+	});
 }
 
 // now using advancedMarkerElement so animation uses css (in the Astro component) and intersection observer
